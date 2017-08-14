9699b9bbdfb98f4672835a03c44b8077d2826b90
Heather Arvidson <heather.arvidson@gmail.com>
2017-08-14 13:03:06 -0700

    adds route to edit a favor request

--- routes/requests.js
+++ routes/requests.js
@@ -18,6 +18,7 @@ const authorize = function(req, res, next) {
   });
 };
 
+//add authorize (and test) once hooked up to frontend
 router.post('/requests', (req, res, next) => {
   return knex('requests')
     .insert({
@@ -35,6 +36,41 @@ router.post('/requests', (req, res, next) => {
       return next(boom.create(500, 'Internal server error.'))
     });
 });
+
+//add authorize once hooked up to frontend
+router.patch('/requests/:id', (req, res, next) => {
+  const favorId = Number.parseInt(req.params.id);
+  console.log(favorId);
+
+  if (Number.isNaN(favorId) || favorId < 0) {
+    return next(boom.create(404, 'Not found--weird id.'));
+  }
+
+  knex('requests')
+    .where('id', favorId)
+    .first()
+    .then((row) => {
+      console.log(row);
+      if (!row) {
+        return next(boom.Create(404, 'Not found--no req by this id.'));
+      }
+
+      return knex('requests')
+        .where('id', favorId)
+        .update({
+          title: req.body.title,
+          description: req.body.description,
+          time_estimate: req.body.timeEstimate,
+          time_window: req.body.timeWindow
+        }, '*')
+    })
+    .then((request) => {
+      res.send(request[0])
+    })
+    .catch((err) => {
+      return next(boom.create(500, 'Internal server error.'))
+    })
+})
 
 
 
