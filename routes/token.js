a8b5e618e1a2fbee37c00615207c1e29947c5da9
Heather Arvidson <heather.arvidson@gmail.com>
2017-08-13 17:32:06 -0700

    create routes

---
+++ routes/token.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const express = require('express');
+const knex = require('../knex');
+const router = express.Router();
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const express = require('express');
+const knex = require('../knex');
+const router = express.Router();
+
+router.post('/users', (req, res, next) => {
+  console.log('POST to /users works');
+
+  //check to see whether user exists
+
+});
