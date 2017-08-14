b0edeacc3f513247b5d6f78860bbf92aa1d5ed3c
Heather Arvidson <heather.arvidson@gmail.com>
2017-08-14 16:32:40 -0700

    includes responder info with dashboard listing of users requests

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,3 @@
 
-//to get all of a user's requests that have not yet been completed; returns an array of objects:
+//get all of a user's requests that have not yet been completed; requests that have received a response will include responder's name, tel, email; returns an array of objects:
 // '/users/:id/requests',
--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.post('/users', (req, res, next) => {
   });
 });
 
-//returns an array containing all requests for that user that have not yet been completed
+//returns an array containing all requests for that user that have not yet been completed; requests that have received a response will include responder's name, tel, email
 router.get('/users/:id/requests', (req, res, next) => {
   const userId = Number.parseInt(req.params.id);
 
@@ -51,15 +51,20 @@ router.get('/users/:id/requests', (req, res, next) => {
   }
 
   knex('requests')
-    .where('user_id', userId)
-    .where('completed', false)
+    .where('requests.user_id', userId)
+    .where('requests.completed', false)
+    .select('completed', 'requests.created_at AS request_created_at', 'requests.updated_at AS request_updated_at', 'description', 'requests.id', 'time_estimate', 'time_window', 'title', 'requests.user_id AS request_user_id', 'responses.user_id AS response_user_id', 'responses.created_at AS response_created_at', 'responses.updated_at AS response_updated_at', 'users.first_name AS response_first_name', 'users.last_name AS response_last_name', 'users.email AS response_email', 'users.tel AS response_tel')
+    .leftJoin('responses', 'requests.id', 'responses.request_id')
+    .leftJoin('users', 'responses.user_id', 'users.id')
     .then((rows) => {
+      console.log(rows);
       if (!rows) {
         return next(boom.create(400, 'Bad request.'));
       }
       res.send(rows);
     })
     .catch((err) => {
+      console.log(err);
       return next(boom.create(500, 'Internal server error.'));
     });
 });
