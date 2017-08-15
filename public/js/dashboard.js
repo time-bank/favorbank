c8722811f62ea97d80bc055014cd72acabc1755a
Heather Arvidson <heather.arvidson@gmail.com>
2017-08-14 18:02:13 -0700

    adds route to delete a request

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,7 @@
 
+
 //get all of a user's requests that have not yet been completed; requests that have received a response will include responder's name, tel, email; returns an array of objects:
-// '/users/:id/requests',
+// $.getJSON (`users/${userID}/requests`),
+
+
+//to delete a request:  DELETE (`/requests/${userID}`
