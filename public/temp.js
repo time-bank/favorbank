e4bbfec84737816664fe6b19b6c2cf8df3f00a83
Daniel Larsson <dl77sea@Daniels-MacBook-Air.local>
2017-08-17 11:07:35 -0700

    prepare the heroku

--- public/temp.js
+++ public/temp.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $(".button-collapse").sideNav();
   });
