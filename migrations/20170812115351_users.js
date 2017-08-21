0aaa559d0cabc3ad81b427dd58ba1379aee27a65
harvidson <heather.arvidson@gmail.com>
2017-08-21 14:49:03 -0700

    tel not nullable

--- migrations/20170812115351_users.js
+++ migrations/20170812115351_users.js
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
     table.string('first_name').notNullable().defaultTo('');
     table.string('last_name').notNullable().defaultTo('');
     table.integer('balance').notNullable().defaultTo(0);
-    table.string('tel').notNullable();
+    table.string('tel');
     table.string('email').notNullable().unique();
     table.specificType('hashed_password', 'char(60)').notNullable();
     table.timestamps(true, true)
